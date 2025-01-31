25563dd8b025153dc4f00a23c4a9ce5ebddbfc96
Aliz Acharya <133593580+aleezacharyax@users.noreply.github.com>
2025-01-31 12:19:56 +0530

    Update settings.py

--- freshdoko/settings.py
+++ freshdoko/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles) 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
